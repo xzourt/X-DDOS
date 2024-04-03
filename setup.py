3ef0e7d44dd7d5bc3d1ed51da746d087d93dd04a
xzourt <160671984+xzourt@users.noreply.github.com>
2024-04-03 21:59:48 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+
+os.system("pip3 install httpx")
+os.system("pip3 install httpx[http2]")
+os.system("pip3 install cloudscraper")
+os.system("pip3 install fake_useragent")
+os.system("pip3 install undetected_chromedriver")
+os.system("pip3 install beautifulsoup4")
+os.system("pip3 install bs4")
+os.system("pip3 install socks")
